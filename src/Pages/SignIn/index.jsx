5854b4b4fcf81169e208190cfb1bd510efa7c43a
Valentina Zapotoczny <valen.czny@gmail.com>
2023-09-21 16:18:30 -0300

    Agregé funcionalidad para saber si el usuario tiene cuenta o no para mostrar los datos y habilitar/deshabilitar botones

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,23 +1,35 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../../Context';
 import { Layout } from '../../Components/Layout';
 
 function SignIn() {
+  const context = useContext(Context);
+
+  //Account
+  const account = localStorage.getItem('account');
+  const parsedAccount = JSON.parse(account);
+  //Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
     return (
       <Layout>
         <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
         <div className='flex flex-col w-80'>
           <p>
             <span className='font-light text-sm'>Email:</span>
-            <span>valen.czny@gmail.com</span>
+            <span>{parsedAccount?.email}</span>
           </p>
           <p>
             <span className='font-light text-sm'>Password:</span>
-            <span>******</span>
+            <span>{parsedAccount?.password}</span>
           </p>
           <Link 
           to='/'>
             <button
-            className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'>
+            className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'
+            disabled={!hasUserAnAccount}>
               Log In
             </button>
           </Link>
@@ -25,13 +37,14 @@ function SignIn() {
             <a href='/' className='font-light text-xs underline underline-offset-4'>Forgot my password</a>
           </div>
           <button
-          className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'>
+          className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'
+          disabled={hasUserAnAccount}>
             Sign up
           </button>
         </div>
       </Layout>
     )
-  }
+}
   
-  export { SignIn }
+export { SignIn }
   
